30f5b76337a55fde02cb142d98b80a39ce69a170
Lucie Gernidos <lgernido@bess-f3r1s10.clusters.42paris.fr>
2023-11-12 18:36:14 +0100

    test

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: lgernido <lgernido@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 17:35:33 by luciegernid       #+#    #+#             */
-/*   Updated: 2023/11/11 19:40:13 by lgernido         ###   ########.fr       */
+/*   Updated: 2023/11/12 18:10:25 by lgernido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,22 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	unsigned int	i;
-	unsigned int	j;
 	unsigned int	dest_length;
-	unsigned int	src_length;
+	unsigned int	src_lenght;
 
-	src_length = ft_strlen(src);
+	src_lenght = ft_strlen(src);
 	dest_length = ft_strlen(dst);
-	j = dest_length;
 	i = 0;
+	if (dest_length >= size)
+		dest_length = size;
 	if (dest_length < size - 1 && size > 0)
 	{
 		while (src[i] && dest_length + i < size - 1)
 		{
-			dst[j] = src[i];
-			j++;
+			dst[dest_length + i] = src[i];
 			i++;
 		}
-		dst[j] = 0;
+		dst[dest_length + i] = 0;
 	}
-	if (dest_length >= size)
-		dest_length = size;
-	if (size == 0)
-		return (0);
-	return (dest_length + src_length);
+	return (dest_length + src_lenght);
 }
