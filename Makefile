44027f9168369b9ad0ae05a45ba4d9199498217d
Lucie Gernidos <lgernido@made-f0Dr11s12.clusters.42paris.fr>
2024-09-23 14:20:01 +0200

    project ready to be presented

--- Makefile
+++ Makefile
@@ -17,21 +17,16 @@ FT_LS = ft_ls/ft_lstadd_back.c ft_ls/ft_lstadd_front.c ft_ls/ft_lstclear.c ft_ls
 ft_ls/ft_lstiter.c ft_ls/ft_lstlast.c ft_ls/ft_lstmap.c ft_ls/ft_lstnew.c ft_ls/ft_lstsize.c
 
 FT_MEM = ft_mem/ft_memchr.c ft_mem/ft_memcmp.c ft_mem/ft_memcpy.c ft_mem/ft_memmove.c ft_mem/ft_memset.c
-
-FT_PRINTF = ft_printf/ft_easyprint.c ft_printf/ft_printf.c ft_printf/ft_printhex.c ft_printf/ft_printptr.c\
-ft_printf/ft_printui.c 
-
+ 
 FT_PUT = ft_put/ft_putchar.c ft_put/ft_putchar_fd.c ft_put/ft_putendl_fd.c ft_put/ft_putnbr_fd.c ft_put/ft_putstr.c 
 
 FT_STR = ft_str/ft_strchr.c ft_str/ft_strdup.c ft_str/ft_striteri.c ft_str/ft_strjoin.c ft_str/ft_strlcat.c ft_str/ft_strlcpy.c \
 ft_str/ft_strlen.c ft_str/ft_strmapi.c ft_str/ft_strncmp.c ft_str/ft_strndup.c ft_str/ft_strnstr.c ft_str/ft_strrchr.c ft_str/ft_strtrim.c\
 ft_str/ft_substr.c 
 
-GNL = get_next_line/get_next_line_char/get_next_line_bonus.c get_next_line/get_next_line_char/get_next_line_utils_bonus.c \
-
 OTHERS = others/ft_atoi.c others/ft_bzero.c others/ft_calloc.c others/ft_itoa.c others/ft_split.c others/ft_tolower.c others/ft_toupper.c
 
-SRC_FILES	= $(FT_IS) $(FT_LS) $(FT_MEM) $(FT_PRINTF) $(FT_PUT) $(FT_STR) $(GNL) $(OTHERS)
+SRC_FILES	= $(FT_IS) $(FT_LS) $(FT_MEM) $(FT_PUT) $(FT_STR) $(OTHERS)
 
 OBJ_FILES	= $(SRC_FILES:.c=.o)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lgernido <lgernido@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 16:26:15 by lgernido          #+#    #+#             */
-/*   Updated: 2023/11/29 18:44:11 by lgernido         ###   ########.fr       */
+/*   Updated: 2024/09/23 14:18:49 by lgernido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,18 +43,6 @@ int					ft_strncmp(const char *s1, const char *s2, size_t n);
 int					ft_atoi(const char *str);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
 int					ft_lstsize(t_list *lst);
-int					ft_printc(int c);
-int					ft_printstr(char *str);
-int					ft_printnb(int n);
-int					ft_printp(void);
-int					ft_nbsize(unsigned int n);
-int					ft_printui(unsigned int n);
-int					ft_hexasize(unsigned int nb);
-int					ft_printhex(unsigned int nb, const char format);
-int					ft_format(va_list ap, const char format);
-int					ft_ptrsize(unsigned long nb);
-int					ft_printptr(unsigned long nb, const char format);
-int					ft_printf(const char *format, ...);
 
 char				*ft_strchr(const char *s, int c);
 char				*ft_strdup(const char *s);
@@ -67,11 +55,8 @@ char				*ft_strjoin(char const *s1, char const *s2);
 char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char				*ft_itoa(int n);
 char				*ft_strtrim(char const *s, char const *set);
-char				*ft_uitoa(unsigned int n);
 char				*ft_strndup(const char *s, size_t n);
-char				*get_next_line(int fd);
-char				*ft_clean_stash(char *line);
-char				*ft_read_line(int fd, char *buffer, char *stash);
+
 
 char				**ft_split(char const *s, char c);
 
@@ -93,8 +78,6 @@ void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
 void				ft_putstr(char *str);
 void				ft_putchar(char c);
-void				ft_writeptr(unsigned long nb, const char format);
-void				ft_writehex(unsigned int nb, const char format);
 
 size_t				ft_strlen(const char *str);
 size_t				ft_strlcat(char *dst, const char *src, size_t size);
